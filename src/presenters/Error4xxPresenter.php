555e5c0ed480fe0ac475988dffdc44fa2da0f95a
filip <filda.kuchar@seznam.cz>
2019-04-13 19:50:04 +0200

    Update error presenters

--- src/presenters/Error4xxPresenter.php
+++ src/presenters/Error4xxPresenter.php
@@ -4,24 +4,32 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
-use Nette;
+use Nette\Application\BadRequestException;
+use Nette\Application\Request;
+use Nette\Application\UI\Presenter;
+use Nette\Bridges\ApplicationLatte\Template;
 
 
-final class Error4xxPresenter extends Nette\Application\UI\Presenter
+final class Error4xxPresenter extends Presenter
 {
 	public function startup(): void
 	{
 		parent::startup();
-		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
+		if ($this->getRequest() !== null && !$this->getRequest()->isMethod(Request::FORWARD)) {
 			$this->error();
 		}
 	}
 
 
-	public function renderDefault(Nette\Application\BadRequestException $exception): void
+	public function renderDefault(BadRequestException $exception): void
 	{
+		$template = $this->getTemplate();
+		if (!$template instanceof Template) {
+			throw new BadRequestException();
+		}
+
 		// load template 403.latte or 404.latte or ... 4xx.latte
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
-		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+		$template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
 	}
 }
--- src/presenters/ErrorPresenter.php
+++ src/presenters/ErrorPresenter.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
-use Nette;
+use Nette\Application\BadRequestException;
+use Nette\Application\Helpers;
+use Nette\Application\IPresenter;
+use Nette\Application\IResponse;
+use Nette\Application\Request;
 use Nette\Application\Responses;
 use Nette\Http;
+use Nette\SmartObject;
 use Tracy\ILogger;
 
 
-final class ErrorPresenter implements Nette\Application\IPresenter
+final class ErrorPresenter implements IPresenter
 {
-	use Nette\SmartObject;
+	use SmartObject;
 
 	/** @var ILogger */
 	private $logger;
@@ -24,18 +29,19 @@ final class ErrorPresenter implements Nette\Application\IPresenter
 	}
 
 
-	public function run(Nette\Application\Request $request): Nette\Application\IResponse
+	public function run(Request $request): IResponse
 	{
 		$exception = $request->getParameter('exception');
 
-		if ($exception instanceof Nette\Application\BadRequestException) {
-			[$module, , $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
+		if ($exception instanceof BadRequestException) {
+			[$module, , $sep] = Helpers::splitName($request->getPresenterName());
 			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
 		}
 
 		$this->logger->log($exception, ILogger::EXCEPTION);
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
-			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
+			$exception = preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'));
+			if ($exception === 1) {
 				require __DIR__ . '/templates/Error/500.phtml';
 			}
 		});
