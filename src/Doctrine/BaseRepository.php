d9b6fce617bd313285b19786c7731a8c8b564d1b
filip <filda.kuchar@seznam.cz>
2020-01-23 02:11:11 +0100

    Composer update, project returned! :)

--- src/Doctrine/BaseRepository.php
+++ src/Doctrine/BaseRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Doctrine;
 
+use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
@@ -19,7 +20,14 @@ abstract class BaseRepository
 	public function __construct(string $class, EntityManagerInterface $entityManager)
 	{
 		$this->entityManager = $entityManager;
-		$this->doctrineRepository = $entityManager->getRepository($class);
+
+		$repository = $entityManager->getRepository($class);
+
+		if (!$repository instanceof EntityRepository) {
+			throw new DBALException('Invalid entity repository!');
+		}
+
+		$this->doctrineRepository = $repository;
 	}
 
 	abstract public function createQueryBuilder(): QueryBuilder;
